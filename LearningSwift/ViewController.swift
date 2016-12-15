72845be86cd82e3e448104d9c23a3f471ef4597f
xuzhiwu <anubis_873375@hotmail.com>
2016-12-15 11:47:14 +0800

    Demo of Learn

---
+++ LearningSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningSwift
+//
+//  Created by 许之午 on 2016/12/15.
+//  Copyright © 2016年 许之午. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
